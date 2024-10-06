da8dfac756ff6e4af7a7e23909a58151a54cb3ed
ab3MN <ab3mn3@gmail.com>
2024-10-06 22:56:08 +0300

    make build

---
+++ index.9f3aac8f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").onclick=function(t){t.preventDefault();var r=t.target.closest(".list-item__link").href;r&&(e.src=r)};
+//# sourceMappingURL=index.9f3aac8f.js.map
